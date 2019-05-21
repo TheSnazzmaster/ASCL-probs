0533b5d94899658ba7409511d8cb508fc7441c9a
Robby Horvath <horvathrobby@gmail.com>
2019-05-21 15:11:46 -0400

    Initialize array method

--- prob10.java
+++ prob10.java
@@ -3,5 +3,21 @@ public class prob10{
         System.out.print("test");
     }
 
+    // initializes and returns the array
+    public static int[][] init(int size, int empt){
+        int[][] arr = new int[size][size];
+        int count = 1;
+        for(int r = 0; r<size; r++){
+            for(int c = 0; c<size; c++){
+                if(empt != count) {
+                    arr[r][c]=count;
+                    count++;
+                }
+                else
+                    arr[r][c] = -1;
+            }
+        }
+        return arr;
+    }
 
 }
